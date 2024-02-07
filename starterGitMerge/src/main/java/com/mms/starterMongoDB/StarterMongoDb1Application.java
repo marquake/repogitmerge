fb54883d155646bd6593bd1013507b457e10f5a8
mms <marcosms@gmail.com>
2024-02-07 21:25:30 +0100

    cambio en main

--- starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
+++ starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarterMongoDb1Application {
 	public static void main(String[] args) {
-		// main: comentario
+		// main: cambio comentario main
 		SpringApplication.run(StarterMongoDb1Application.class, args);
 	}
 
