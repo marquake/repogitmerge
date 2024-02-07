7758c10bc2df1350b5e009814b6acbd31bba80d1
mms <marcosms@gmail.com>
2024-02-07 21:24:03 +0100

    cambio en feature one

--- starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
+++ starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarterMongoDb1Application {
 	public static void main(String[] args) {
-		// main: comentario
+		// main: feature one change
 		SpringApplication.run(StarterMongoDb1Application.class, args);
 	}
 
