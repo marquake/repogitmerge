c848443d7edf4f34183e7fec1c704d936e731cfd
mms <marcosms@gmail.com>
2024-02-07 21:02:49 +0100

    primera subida

---
+++ starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
@@ -0,0 +1,13 @@
+package com.mms.starterMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterMongoDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterMongoDb1Application.class, args);
+	}
+
+}
