3c8d1cd198649194bcd09796b75cbe4b710ff536
marquake <marcosms@gmail.com>
2024-02-07 21:15:52 +0100

    Update StarterMongoDb1Application.java

--- starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
+++ starterGitMerge/src/main/java/com/mms/starterMongoDB/StarterMongoDb1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StarterMongoDb1Application {
-	// a: github
 	public static void main(String[] args) {
 		SpringApplication.run(StarterMongoDb1Application.class, args);
 	}
